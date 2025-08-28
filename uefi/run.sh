c1bc7395f440e73cde12df661e1084ec803aab91
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-08-28 13:03:22 +0200

    VGA setting
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- uefi/run.sh
+++ uefi/run.sh
@@ -3,8 +3,7 @@ qemu-system-x86_64 -m 256 \
   -drive if=pflash,format=raw,readonly=on,file=./OVMF_CODE.fd \
   -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
   -machine q35 \
-  -vga std \
   -device qemu-xhci \
   -drive id=usbstick,if=none,format=raw,file=usb_uefi.img \
   -device usb-storage,drive=usbstick,removable=on
-
+#  -vga std \
